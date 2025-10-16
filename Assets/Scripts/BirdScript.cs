2b7946adbdf4865a9a5c235c9b837c4e92adbeb2
thisgur03-glitch <thisgur03@gmail.com>
2025-10-16 14:10:21 +0530

    added bird wings

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -6,32 +6,66 @@ public class BirdScript : MonoBehaviour
 {
     public LogicScript logicScript;
     private Rigidbody2D rigidbody2D;
-    private bool isGrounded;
     public float jumpForce = 20f;
     public bool birdIsAlive = true;
 
-    // Start is called before the first frame update
+    [Header("Wing Sprites")]
+    public GameObject wingDown;
+    public GameObject wingUp;
+
+    private bool isWingUp = false;
+
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
-
+        SetWingState(false); // Start with down wing
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Jump();
-    }
+        if (!birdIsAlive) return;
 
-    void Jump(){
-        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive == true){
-            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+        // Jump if Spacebar pressed, mouse clicked, or screen tapped
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+            SetWingState(true);
+        }
+
+        // Return wing to down when key/tap is released
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
+        {
+            SetWingState(false);
+        }
+
+        // Optional: handle mobile touch separately
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Jump();
+            SetWingState(true);
+        }
+        else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            SetWingState(false);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision){
+    void Jump()
+    {
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         logicScript.gameOver();
         birdIsAlive = false;
+    }
+
+    void SetWingState(bool up)
+    {
+        isWingUp = up;
+        wingUp.SetActive(up);
+        wingDown.SetActive(!up);
     }
 }
